69bf3cf6504d447136428f067a04acdb7400ade1
Maher Zubair <130863730+Mehar-Zubi@users.noreply.github.com>
2023-08-10 18:43:19 +0500

    Mehar Zubair

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,4 +1,4 @@
-const {cmd } = require('../lib')
+const { Insta,cmd , Config , prefix } = require('../lib')
 cmd({
         pattern: "insta",
         desc: "download instagram post.",
@@ -6,10 +6,34 @@ cmd({
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
-        const { Insta } = require('../lib')
 if(!text) return citel.reply('Give Me Insta Post Link')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[i], `*╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ᴏᴘᴇɴ ᴀⁱ* ♕`, citel)
 }
     });
+
+//----------------------------------------------------------------------------------
+cmd({
+    pattern: "insta2",
+    desc: "Downloads Instagram videos.",
+    category: "downloader",
+    filename: __filename,
+    use: '<add fb url.>'
+},
+async(Void, citel, text) => {
+if (!text || !text.startsWith("insta2 ")) {
+    await citel.send("Please give me Insta Video Url.\nExample: .insta2 https://www.instagram.com/reel/Cmvj5aWJE56/?utm_source=ig_web_copy_link_*");
+}
+
+try {
+    const { instagram } = require('instagram-downloader');
+    let insta = await instagram(text);
+
+    for (let i = 0; i < insta.length; i++) {
+        await Void.sendFileUrl(citel.chat, insta[i], Config.send + Config.caption, citel);
+    }
+} catch (error) {
+    console.log("Error while Fb Downloading:", error);
+    await citel.send("*_Error, Video Not Found_*");}
+})
