3394d0b398f582942cac3fc6f147e5c205ecb546
Maher Zubair <130863730+Mehar-Zubi@users.noreply.github.com>
2023-08-14 14:39:06 +0500

    Mehar Zubair

--- commands/koyeb+heroku.js
+++ commands/koyeb+heroku.js
@@ -8,44 +8,68 @@
  * @description : ᴏᴘᴇɴ ᴀⁱ ♕,A Multi-functional whatsapp bot.
  * @version 1.0.3 
  **/
-const DB = require('../lib/scraper')
 const axios = require('axios');
-const { tlang, prefix,cmd } = require('../lib')
+const { tlang,cmd } = require('../lib')
 const Config = require('../config')
 const { redeploy , getvar , delvar , getallvar , change_env , get_deployments} = require('../lib/koyeb')
-const simpleGit = require('simple-git');
-const git = simpleGit();
-const Heroku = require('heroku-client');
+
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
-if(Config.HEROKU_APP_NAME && Config.HEROKU_API_KEY )
-{
-        
-     cmd({
-                 pattern: "updatnow",
-                 alias: ["unow"],
-                 desc: "Shows repo\'s refreshed commits.",
-                 category: "tools",
-                 filename: __filename
-             },
-        async(Void, citel, text,{ isCreator }) => {
-                if(!isCreator) return await citel.reply("Only Owner Can Use This Command")
-                let commits = await DB.syncgit()
-                if (commits.total === 0) return await citel.reply(`*YOU HAVE LATEST VERSION INSTALLED!*`)
-                let update = await DB.sync()
-                let buttonMessaged = 
-                {
-                     text:" *> Please Wait Updater Started...!*\n  *───────────────────────────*\n"+update +"\n  *───────────────────────────*",
-                     footer: 'UPDATER --- sᴜʜᴀɪʟ ᴛᴇᴄʜ ɪɴғᴏ \n www.youtube.com/c/SuhailTechInfo',
-                     headerType: 4,
-                };
-                await Void.sendMessage(citel.chat, buttonMessaged);
-                await require("simple-git")().reset("hard",["HEAD"])
-                await require("simple-git")().pull()
-                await citel.reply("*Successfully updated. Now You Have Latest Version Installed!*")
-                process.exit(0);
-       })
-}
+cmd(
+  {
+    pattern: "updatenow",
+    alias: ["unow"],
+    desc: "update bot with main repo.",
+    filename: __filename,
+    category: "misc",
+  },
+  async (Void,citel,text,{isCreator}) => {
+    async function updatedb() {	
+      const simpleGit = require('simple-git')	
+          const git = simpleGit();	
+        const Heroku = require('heroku-client');	
+        const heroku = new Heroku({ token: process.env.HEROKU_API_KEY })	
+          await git.fetch();	
+              var commits = await git.log(['main' + '..origin/' +'main']);	
+              if (commits.total === 0) {	
+                return 'ʏᴏᴜ..ʜᴀᴠᴇ...ᴀʟʀᴇᴅʏ..ᴜᴘᴅᴀᴛᴇᴅ...'	
+              } else {	
+                    var app = await heroku.get('/apps/' + process.env.HEROKU_APP_NAME)	
+                   //   await Void.sendMessage(citel.chat,{text:'*ᴜᴘᴅᴀᴛɪɴɢ...*'})	
+                  git.fetch('upstream', 'main');	
+                  git.reset('hard', ['FETCH_HEAD']);	
+      
+                  var git_url = app.git_url.replace(	
+                    "https://", "https://api:" + process.env.HEROKU_API_KEY + "@"	
+                  )   	
+                  try {	
+                    await git.addRemote('heroku', git_url);	
+                  } catch { console.log('heroku remote adding error'); }	
+                  await git.push('heroku', 'main');	
+      
+                  return '*ʙᴏᴛ ᴜᴘᴅᴀᴛᴇᴅ...*\n_Restarting._'	
+      
+      
+              }	
+            }	
+       if(!isCreator) return citel.reply(tlang().owner);
+       if (Config.heroku=true){
+        const DB = require('../lib')
+        let commits = await DB.syncgit()
+        if (commits.total === 0)  {
+         citel.reply(`Hey ${citel.pushName}. You have latest version installed.`)
+          } else { 
+             citel.reply('Build Started...')
+            let update = await DB.updatedb()
+              citel.reply(update)
+          }
+
+       }
+       let check = await get_deployments()
+       if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
+       let data = await redeploy();
+       return citel.reply(data)
+  })
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 cmd(
   {
