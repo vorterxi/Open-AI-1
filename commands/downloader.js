212534005d50dbaf459c5f001349b4b2e0a20dc2
Maher Zubair <130863730+Mehar-Zubi@users.noreply.github.com>
2023-08-14 08:38:30 +0500

    Mehar Zubair

--- commands/downloader.js
+++ commands/downloader.js
@@ -67,7 +67,7 @@ cmd({
         },
 
         async(Void, citel, text) => {
-        if(!text )return citel.reply("*_Give me App Name_*");
+        if(!text )return citel.reply("*Give me App Name*");
 
 	const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`; };
 	let randomName = getRandom(".apk");
@@ -76,18 +76,18 @@ cmd({
 	let searc = await search(text);          //console.log(searc);
 	let data={};
 	if(searc.length){ data = await download(searc[0].id); }
-	else return citel.send("*_APP not Found, Try Other Name_*");
+	else return citel.send("*APP not Found, Try Other Name*");
 	
 	
 	const apkSize = parseInt(data.size);
-	if(apkSize > 100) return citel.send(`❌ File size bigger than 150mb.`);
+	if(apkSize > 200) return citel.send(`❌ File size bigger than 250mb.`);
        const url = data.dllink;
 	 let  inf  ="*App Name :* " +data.name;
          inf +="\n*App id        :* " +data.package;
-         inf +="\n*Last Up       :* " +data.lastup;
+         inf +="\n*Last Update       :* " +data.lastup;
          inf +="\n*App Size     :* " +data.size;
-        // inf +="\n*App Link     :* " +data.dllink;
-	inf +="\n\n "+ Config.caption
+         inf +="\n*App Link     :* " +data.dllink;
+	 inf +="\n\n "+ Config.caption
          
 
 axios.get(url, { responseType: 'stream' })
@@ -117,7 +117,7 @@ axios.get(url, { responseType: 'stream' })
       if (err) { console.error('Error deleting file:', err); } else { console.log('File deleted successfully'); } });
   }) .catch(error => {
 	fs.unlink(filePath)
-    return citel.send('*_Apk not Found, Sorry_*')//:', error.message);
+    return citel.send('*Apk not Found, Sorry*')//:', error.message);
   });
 	
 	
