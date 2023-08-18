26da065f7f85711191093a3acbb03a103de8f917
Maher Zubair <130863730+Mehar-Zubi@users.noreply.github.com>
2023-08-18 17:46:00 +0500

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -100,7 +100,7 @@ cmd({
 async(Void, citel,text) => {
     let zx = text.length;
     if (zx < 8) {
-        let {data} = await axios.get(`http://api.brainshop.ai/get?bid=177318&key=jj9hZmtlwELzCvJP&uid=[${citel.sender.split("@")[0]}]&msg=[${text}]`);
+        let {data} = await axios.get(`http://api.brainshop.ai/get?bid=177318&key=jj9hZmtlwELzCvJP&uid=[${citel.sender.split("@")[0]}]&msg=hi im`);
         return citel.reply(data.cnt);  
     }
     if (!text) return citel.reply(`ʜᴇʏ ᴛʜᴇʀᴇ! ${citel.pushName}. ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ ᴅᴏɪɴɢ ᴛʜᴇsᴇ ᴅᴀʏs?`);
