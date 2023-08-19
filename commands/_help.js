b7d713ab6555fe61a9ad099d64f13b3c090bb8eb
Maher Zubair <130863730+Mehar-Zubi@users.noreply.github.com>
2023-08-19 20:19:25 +0500

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -76,7 +76,7 @@ sɪɢᴍᴀ_ᴍᴅ.cmd({
 ` + ''
                 for (const category in cmds) 
                 {
-                   str += `┌──『 ${tiny(category)} 』──❖\n`${readmore} ;
+                   str += `┌──『 ${tiny(category)} 』──❖${readmore}\n` ;
                    if(text.toLowerCase() == category.toLowerCase()){ str = `╭─────❏ ${tiny(category)} ❏\n` ;      
                         for (const plugins of cmds[category]) { str += `│ ${fancytext(plugins,1)}\n` ; }
                         str += `└────────────◉\n`  ;
