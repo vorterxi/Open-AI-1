a26040f6ff66d3b44a4e79c81f3946d9e8476cc4
Maher Zubair <130863730+Mehar-Zubi@users.noreply.github.com>
2023-08-19 22:06:55 +0500

    Mehar Zubair

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ global.gurl = 'https://instagram.com/' // add your username
 global.sudo = process.env.SUDO || '923'
 global.devs = '923';
 global.waUrl = "https://chat.whatsapp.com/Ckwh1CMUjXV1Vq5JCBycCK",
+global.Gname = "xxx",
 global.website = 'https://github.com' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/t6VBCJL.jpg'
 module.exports = {
