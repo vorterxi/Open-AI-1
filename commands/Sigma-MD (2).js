43315edab7788d37e2f8e826e9a7faafc71f936c
Maher Zubair <130863730+Mehar-Zubi@users.noreply.github.com>
2023-08-23 23:53:57 +0500

    Update Sigma-MD (2).js

--- commands/Sigma-MD (2).js
+++ commands/Sigma-MD (2).js
@@ -9,13 +9,13 @@ smd({
     async(Void, citel, text,) => {
 
         await Void.sendMessage(citel.chat, {
-           // image: textt,
-            text: `┏━━⟪⟪ 🅼♥︎❚❚♥︎🆉 ⟫━⦿\n┃✗ *•ᴅᴇᴠᴇʟᴏᴘᴇʀ ɴᴀᴍᴇ•* \n┃✗ *•ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ•* \n┃✗ *•ɴᴜᴍʙᴇʀ•* https://wa.me/923466319114\n┗━━━━━━━━━━⦿\n            *•ᴘᴏᴡᴇʀᴇᴅ ʙʏ•* sɪɢᴍᴀ ᴹᴰ`,
+            image: `${devp}`,
+            text: `┏━━⟪⟪ 🅼♥︎❚❚♥︎🆉 ⟫━⦿\n┃✗ *•ᴅᴇᴠᴇʟᴏᴘᴇʀ ɴᴀᴍᴇ•* \n┃✗ *•ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ•* \n┃✗ *•ɴᴜᴍʙᴇʀ•* https://wa.me/923466319114\n┗━━━━━━━━━━⦿\n          *•ᴘᴏᴡᴇʀᴇᴅ ʙʏ•* sɪɢᴍᴀ ᴹᴰ`,
 	    contextInfo: {
 	        externalAdReply: {
                 title: `ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ`,
                 body: "ᴛᴀᴘ ʜᴇʀᴇ ᴛᴏ ᴍᴇssᴀɢᴇ ᴍᴇ",
-               // thumbnail: textt,
+                thumbnail: `${devp}`,
                 mediaType: 4,
                 mediaUrl: '',
                 sourceUrl: `https://wa.me/923466319114`,}}
