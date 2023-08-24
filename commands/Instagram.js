8b8e8de00c7f136c1cc4e4f66f84a0c5d8dafcaa
Maher Zubair <130863730+Mehar-Zubi@users.noreply.github.com>
2023-08-24 07:18:45 +0500

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,5 +1,7 @@
-const { Insta, cmd , Config , prefix } = require('../lib')
-cmd({
+const { smd , prefix } = require('../lib')
+const sigma_config = require('../config')
+const ig = require('instagram-url-dl')
+smd({
         pattern: "insta",
         desc: "download instagram post.",
         category: "downloader",
@@ -7,8 +9,8 @@ cmd({
     },
     async(Void, citel,text,) => {
 if(!text) return citel.reply('ɢɪᴠᴇ ᴍᴇ ɪɴsᴛᴀ ᴘᴏsᴛ ʟɪɴᴋ')
-let response = await Insta(text)
+let response = await ig(text)
 for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[i], `╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 sɪɢᴍᴀ ᴹᴰ`, citel)
+await Void.sendFileUrl(citel.chat, response[i], `╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${sigma_config.botname}`, citel)
 }
     });
